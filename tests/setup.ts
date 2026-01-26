eb59c24d056dde8bc9850f200485308eec2d8ad7
Shrey <shreyshingala1@gmail.com>
2026-01-26 16:00:23 -0500

    Fixed merge conflicts

--- tests/setup.ts
+++ tests/setup.ts
@@ -3,12 +3,11 @@ import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
 
 globalThis.ResizeObserver = class ResizeObserver {
-    observe() {}
-    unobserve() {}
-    disconnect() {}
+  observe() {}
+  unobserve() {}
+  disconnect() {}
 }
 
-// runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
   cleanup();
 })
