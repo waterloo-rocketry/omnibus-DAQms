856a44587a72edfaffdc80ebfc5e2b94864e8407
Sophia Xu <xu.sophia.sx@gmail.com>
2026-02-21 20:56:35 -0500

    deleted original dashboard plot hard-coding

--- src/components/SensorMonitoringDashboard.tsx
+++ src/components/SensorMonitoringDashboard.tsx
@@ -36,48 +36,4 @@ export const SensorMonitoringDashboard = () => {
           ))}
     </div>
   );
-
-  //   <div className="mb-4">
-  //     <div className="flex flex-col md:flex-row gap-4 mb-4">
-  //       <div className="flex-1 min-w-0">
-  //         <LineGraph
-  //           channelName={channels[0].name}
-  //           title={channels[0].title}
-  //         />
-  //       </div>
-  //       <div className="flex-1 min-w-0">
-  //         <LineGraph
-  //           channelName={channels[1].name}
-  //           title={channels[1].title}
-  //         />
-  //       </div>
-  //       <div className="flex-1 min-w-0">
-  //         <LineGraph
-  //           channelName={channels[2].name}
-  //           title={channels[2].title}
-  //         />
-  //       </div>
-  //     </div>
-  //     <div className="flex flex-col md:flex-row gap-4">
-  //       <div className="flex-1 min-w-0">
-  //         <LineGraph
-  //           channelName={channels[3].name}
-  //           title={channels[3].title}
-  //         />
-  //       </div>
-  //       <div className="flex-1 min-w-0">
-  //         <LineGraph
-  //           channelName={channels[4].name}
-  //           title={channels[4].title}
-  //         />
-  //       </div>
-  //       <div className="flex-1 min-w-0">
-  //         <LineGraph
-  //           channelName={channels[5].name}
-  //           title={channels[5].title}
-  //         />
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
 };
