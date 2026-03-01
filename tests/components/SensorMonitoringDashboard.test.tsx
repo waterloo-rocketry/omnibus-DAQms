d5938ea9d7de6a1ad662ce9706f942eab6d0ec65
Sophia Xu <xu.sophia.sx@gmail.com>
2026-03-01 13:12:03 -0500

    added integration ui test for sensor monitoring dashboard

--- tests/components/SensorMonitoringDashboard.test.tsx
+++ tests/components/SensorMonitoringDashboard.test.tsx
@@ -1,3 +1,11 @@
-import { describe, it, expect, beforeEach } from 'vitest'
-import { render, screen, waitFor } from '@testing-library/react'
+import { describe, it, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
 import { SensorMonitoringDashboard } from '@/components/SensorMonitoringDashboard/SensorMonitoringDashboard'
+
+describe ('Sensor Monitoring Dashboard', () => {
+    it ('renders 6 plots', () => {
+        render(<SensorMonitoringDashboard/>);
+        const graphs = screen.getAllByRole('img');
+        expect(graphs).toHaveLength(6);
+    })
+})
