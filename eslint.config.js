310463a69581baa007694ace439e1851da20a5d3
Chris Yang <chris.yang@shopify.com>
2026-04-07 14:28:04 -0400

    fix linter settings

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-    globalIgnores(['dist', 'src/components/ui']),
+    globalIgnores(['dist', 'src/components/ui', 'coverage']),
     {
         files: ['**/*.{ts,tsx}'],
         extends: [
@@ -18,6 +18,17 @@ export default defineConfig([
         languageOptions: {
             ecmaVersion: 2020,
             globals: globals.browser,
+        },
+        rules: {
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '_',
+                    varsIgnorePattern: '_',
+                    caughtErrorsIgnorePattern: '_',
+                    destructuredArrayIgnorePattern: '_',
+                },
+            ],
         },
     },
 ])
